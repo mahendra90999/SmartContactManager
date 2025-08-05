1ce760e63d78720cdc81784a167462b5da43df9e
Techblog-java-project <mahendravpatil3@gmail.com>
2025-08-05 22:34:01 +0530

    correcting "spring.jpa.hibernate.ddl-auto=update" parameter;
    creating two entities user and contact and check that working or not
    
    makeing a userrepository dao class for crud operation on database.
    
    check the working of database and connection useing homecontroller
    class.

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.smart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @RestController
 public class HomeController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping("/")
-	public String HomeHandler(){
+	public String test(){
+		
+		
+		User user = new User();
+		user.setName("mahendra");
+		user.setEmail("mahendra@mai.com");
+		
+		userRepository.save(user);
 		
 		return "Hii this is home handler";
 	}
