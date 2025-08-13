d6f76b646af84dcd0bcc51b957f89c8468808446
Techblog-java-project <mahendravpatil3@gmail.com>
2025-08-13 22:40:59 +0530

    1. Modify the signUp handler method: Update the signUp handler to include a User object so that user information can be populated into the form fields.
    2. Create a registerUser POST handler method:
    Implement a new registerUser method to handle form submissions from the
    signup page. This method should:
    
    Extract the user information from the form.
    
    Save the user data into the database.
    
    Use a try-catch block to handle any exceptions that occur during
    registration.
    
    Store error or success messages in the HttpSession to display them as
    alerts on the signup page.
    
    3. Create a Message class in the helper package:
    Add a Message class to the helper package. This class will be used to
    create structured alert messages (such as success, error, or warning) to
    be displayed on the web page.

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.smart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Messages;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("/")
 	public String Home(Model model) {
@@ -26,6 +39,44 @@ public class HomeController {
 	public String signUp(Model model) {
 		
 		model.addAttribute("title","Register new User");
+		model.addAttribute("user",new User());
+		return "signup";
+	}
+	
+	@RequestMapping(value="/do_register",method = RequestMethod.POST )
+	public String registerUser(@ModelAttribute("user") User user,@RequestParam(value="aggriment",defaultValue = "false") boolean agriment,Model model,HttpSession session) {
+		
+		try {
+			if(!agriment) {
+				
+				System.out.println("You have not aggred the terms and conditions.");
+				throw new Exception("You have not aggred the terms and conditions.");
+			}
+			
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.jpg");
+			
+			System.out.println(agriment);
+			System.out.println(user);
+			
+			User result = this.userRepository.save(user);
+			
+			
+			
+			model.addAttribute("user",result);
+			
+			model.addAttribute("user",new User());
+			
+			session.setAttribute("message",new Messages("successfully register a new user","alert-success"));
+			return "signup";
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			model.addAttribute("user",user);
+			session.setAttribute("message",new Messages("something Went wrong..!!"+e.getMessage(),"alert-danger"));
+		}
+		
 		return "signup";
 	}
 }
---
+++ src/main/java/com/smart/helper/Messages.java
@@ -0,0 +1,30 @@
+package com.smart.helper;
+
+public class Messages {
+	
+	private String content;
+	private String type;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Messages() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Messages(String content, String type) {
+		super();
+		this.content = content;
+		this.type = type;
+	}
+	
+}
