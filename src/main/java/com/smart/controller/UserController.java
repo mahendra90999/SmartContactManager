7e8bf19b798897fa1feadde4cccdc89d9fc89ab8
Techblog-java-project <mahendravpatil3@gmail.com>
2025-08-27 22:40:00 +0530

    feat: render user contact list on HTML page with database data and apply CSS styling

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
+import com.smart.dao.contactRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
 import com.smart.helper.Messages;
@@ -28,6 +30,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private contactRepository contactRepository;
 	
 //method for adding common data for handler methods
 	@ModelAttribute
@@ -128,5 +133,22 @@ public class UserController {
 		
 		return("normal/add_contact_form");
 	}
+	
+	
+	@GetMapping("/show_contacts")
+	public String showContacts(Model model,Principal principal) {
+		
+		String username = principal.getName();
+		User user = userRepository .getUserByname(username);
+		
+		List<Contact> contacts = contactRepository.findContactByUser(user.getId());
+		
+		model.addAttribute("contacts",contacts);
+		System.out.println(contacts);
+		return "normal/show_contacts";
+	}
+	
+	
+	
 	
 }
